9616fdef09a5b142195ab78dd28569579e1c7973
Joshua Lee <joshuamorganlee@gmail.com>
2016-11-21 10:24:11 -0600

    displaying items in main page

---
+++ app/src/main/java/com/example/joshualee/plantapp/Plant.java
@@ -0,0 +1,27 @@
+package com.example.joshualee.plantapp;
+
+/**
+ * Created by joshualee on 11/21/16.
+ */
+
+public class Plant {
+    String name;
+    String sciName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSciName() {
+        return sciName;
+    }
+
+    public void setSciName(String sciName) {
+        this.sciName = sciName;
+    }
+
+}
