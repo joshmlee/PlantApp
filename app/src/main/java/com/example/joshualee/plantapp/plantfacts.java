b1d44346d53cbcede1ddb98c02969142035c01a7
KelseyOlson <Kelsey.olson@drake.edu>
2016-11-21 10:35:14 -0600

    plant facts and wellness activities

---
+++ app/src/main/java/com/example/joshualee/plantapp/plantfacts.java
@@ -0,0 +1,13 @@
+package com.example.joshualee.plantapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class plantfacts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plantfacts);
+    }
+}
