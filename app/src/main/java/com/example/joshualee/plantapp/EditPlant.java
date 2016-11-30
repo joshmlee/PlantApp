855fa9428cf4009eae319a7fa15d7cb733fba24b
Jennifer Steffens <jennifer.steffens@drake.edu>
2016-11-30 10:23:06 -0600

    Attempted to fix the gallery shpeel

--- app/src/main/java/com/example/joshualee/plantapp/EditPlant.java
+++ app/src/main/java/com/example/joshualee/plantapp/EditPlant.java
@@ -7,12 +7,13 @@ import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class AddPlant extends AppCompatActivity {
+public class EditPlant extends AppCompatActivity {
 
     private static int RESULT_LOAD_IMG = 1;
     String imgDecodableString;
@@ -20,23 +21,32 @@ public class AddPlant extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_plant_add);
+        setContentView(R.layout.activity_edit_plant);
     }
     public void loadImagefromGallery(View view) {
         // Create intent to Open Image applications like Gallery, Google Photos
         Intent galleryIntent = new Intent(Intent.ACTION_PICK,
                 android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        galleryIntent.setType("image/*");
+        galleryIntent.setAction(Intent.ACTION_GET_CONTENT);
         // Start the Intent
         startActivityForResult(galleryIntent, RESULT_LOAD_IMG);
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        Toast.makeText(this, "banana",
+//                Toast.LENGTH_LONG).show();
         super.onActivityResult(requestCode, resultCode, data);
         try {
             // When an Image is picked
+            Toast.makeText(this, "You have picked a banana",
+                    Toast.LENGTH_LONG).show();
             if (requestCode == RESULT_LOAD_IMG && resultCode == RESULT_OK
                     && null != data) {
                 // Get the Image from data
+
+                Toast.makeText(this, "You have picked an image",
+                        Toast.LENGTH_LONG).show();
 
                 Uri selectedImage = data.getData();
                 String[] filePathColumn = { MediaStore.Images.Media.DATA };
