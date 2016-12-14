778b0a6e3f49c8cba26c60147ebc6b40cea57d49
KelseyOlson <Kelsey.olson@drake.edu>
2016-12-13 18:45:57 -0600

    Plant Facts and Wellness home buttons

--- app/src/main/java/com/example/joshualee/plantapp/plantwellness.java
+++ app/src/main/java/com/example/joshualee/plantapp/plantwellness.java
@@ -1,7 +1,10 @@
 package com.example.joshualee.plantapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class plantwellness extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class plantwellness extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plantwellness);
+
+        ImageView homeButton = (ImageView) findViewById(R.id.imageView3);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent3 = new Intent(plantwellness.this, MainActivity.class);
+                startActivity(intent3);
+            }
+        });
     }
 }
